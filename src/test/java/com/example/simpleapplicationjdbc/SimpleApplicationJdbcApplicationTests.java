97bb9e2ffa5af2b9ce5409efaf4565c40937148a
WoldemarK <kovtynov.vladimir@gmail.com>
2023-04-03 19:24:31 +0300

    Добавил JUnit написал проверочный тест @DisplayName("find-All-Users")

---
+++ src/test/java/com/example/simpleapplicationjdbc/SimpleApplicationJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleapplicationjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
