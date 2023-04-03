3dacbeaf884ae1523e942dc66ca56f101c257a80
WoldemarK <kovtynov.vladimir@gmail.com>
2023-04-03 18:41:09 +0300

    Добавил логирование

--- src/main/java/com/example/simpleapplicationjdbc/dao/StudentJDBCTemplate.java
+++ src/main/java/com/example/simpleapplicationjdbc/dao/StudentJDBCTemplate.java
@@ -2,6 +2,7 @@ package com.example.simpleapplicationjdbc.dao;
 
 import com.example.simpleapplicationjdbc.entity.Student;
 import com.example.simpleapplicationjdbc.mapper.StudentMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Slf4j
 @Repository
+@RequiredArgsConstructor
 public class StudentJDBCTemplate implements StudentDAO {
     @Value("${LISTS_STUDENTS}")
     private String getAllStudent;
@@ -26,9 +28,6 @@ public class StudentJDBCTemplate implements StudentDAO {
     private String GET_NAME_BY_ID_STUDENT;
     public final JdbcTemplate jdbcTemplate;
 
-    public StudentJDBCTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     @Override
     public void create(Student student) {
@@ -37,6 +36,7 @@ public class StudentJDBCTemplate implements StudentDAO {
 
     @Override
     public Student findByStudentId(Integer id) {
+        log.info("Поиск по ID произведен " + id);
         return jdbcTemplate.queryForObject
                 (
                         GET_ID_STUDENT,
@@ -47,6 +47,7 @@ public class StudentJDBCTemplate implements StudentDAO {
 
     @Override
     public List<Student> findAll() {
+        log.info("Произведен показ всех студентов");
         return jdbcTemplate.query
 
                 (
@@ -57,16 +58,19 @@ public class StudentJDBCTemplate implements StudentDAO {
 
     @Override
     public void update(Integer id, Integer age) {
+        log.info("Произведено обновление данных о студенте " + id + " " + age);
         jdbcTemplate.update(UPDATE, age, id);
     }
 
     @Override
     public void delete(Integer id) {
+        log.info("Произведено удаление студента" + id);
         jdbcTemplate.update(DELETE_STUDENT, id);
     }
 
     @Override
     public String findStudentNameById(Long id) {
+        log.info("Поиск имени студента по идентификатору ID " + id);
         return jdbcTemplate.queryForObject
                 (
                         GET_NAME_BY_ID_STUDENT,
